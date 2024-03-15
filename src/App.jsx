de329ca18c86954b06998542fa25fe8a8ad3343f
Roman Fedevych <fedevychr@gmail.com>
2024-03-15 02:46:17 +0200

    task-4

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
 import article from './data/article.json'
 import stats from './data/stats.json'
 import forbes from './data/forbes.json'
+import transactions from './data/transactions.json'
 
 export const App = () => {
   return (
@@ -35,7 +36,8 @@ export const App = () => {
         />
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory  />
+        <CryptoHistory
+          transactions={transactions} />
       </Container>
     </Section>
   );
